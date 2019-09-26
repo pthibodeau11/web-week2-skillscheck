d78c25310af7310bf4d20d625fcba509e1fb227e
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-09-26 11:45:22 -0500

    1145 push

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -135,12 +135,18 @@ function bigOrSmall(arr) {
 
 //////////////////PROBLEM 13////////////////////
 
-// Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. Finally, return the new reversed array
+// Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. 
+// Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. 
+// Finally, return the new reversed array
 
 //Code Here
 
-function arrayReverser(){
-	
+function arrayReverser(arr){
+	let reversed = [];
+	for(let i = arr.length - 1; i >= 0; i--){
+		reversed.push(arr[i])
+	}
+	return reversed;	
 }
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
@@ -154,31 +160,45 @@ const myNumbers = [3, 55, 788, 2, 1]
 // First, let's use .map to create a new array that doubles each number. Call the new array 'doubled'
 
 // Code Here
+const doubled = myNumbers.map(function(num){return num * 2});
 
 //////////////////PROBLEM 15////////////////////
 
 // Now lets use .filter to only get the nubmer(s) greater than 100. Call the new array 'filtered'
 
 // Code Here
+const filtered = myNumbers.filter(function(num){return num > 100});
 
 //////////////////PROBLEM 16////////////////////
 
 // Next up, reduce to get the total of the array. call the new array 'total'
 
 // Code Here
+const total = myNumbers.reduce(function(total, num){return total += num})
 
 //////////////////PROBLEM 17////////////////////
 
-// Last, lets use .forEach to find the index of each item in the array. First, create an empty array called myNumbersIndex. Next, using forEach, push each items index from the myNumbers array to the myNumbersIndex array
+// First, create an empty array called myNumbersIndex. 
+// Next, using forEach, push each items index from the myNumbers array to the myNumbersIndex array
+// Last, lets use .forEach to find the index of each item in the array. 
 
 // Code Here
-
+let myNumbersIndex = []
+myNumbers.forEach(function(num, index){
+	myNumbersIndex.push(index)
+})
 //////////////////PROBLEM 18////////////////////
 
-// Did you know that George Foreman has five sons named George? Let's go ahead and change everyone's name in the notGeorge array to George using .map. Call the new array 'forTheLoveOfGeorge'
+// Did you know that George Foreman has five sons named George? 
+// Let's go ahead and change everyone's name in the notGeorge array to George using .map. 
+// Call the new array 'forTheLoveOfGeorge'
+
 const notGeorge = ['Louis', 'Ted', 'Bill', 'Sharon', 'Mark', 'Angela']
 
 // Code Here
+const forTheLoveOfGeorge = notGeorge.map(function(name){
+	return name = "George"
+})
 
 //////////////////PROBLEM 19////////////////////
 
@@ -193,9 +213,15 @@ const people = [
 ]
 
 // Code Here
+const enemies = people.filter(function(element){
+	return element.friend === false
+})
 
 //////////////////PROBLEM 20////////////////////
 
 // Using the people array above, let's get a total of the awesomeLevel from all the people. Call the new array 'totallyAwesome'. Use .reduce()
 
 // Code Here
+const totallyAwesome = people.reduce(function(total, level){
+	return total += level.awesomeLevel;
+})
--- javascript-2/student.js
+++ javascript-2/student.js
@@ -5,26 +5,38 @@
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
 // Code Here
-
+let lovesCode = true;
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
 
 // Code Here
-
+var age = 32;
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
 // Code Here
-
+var name = "Pat"
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
 // Code Here
-
+var phrase = ""
 //////////////////STEP 5////////////////////
-//Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
+//Create an if statement that checkes to see if the lovesCode variable is equal to true, 
+// if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
+// if(lovesCode !== true){
+//     return lovesCode = "I do not love to code"
+// } else {return lovesCode = "I love code"}
 
+
+function checkLovesCode(lovesCode){
+if (lovesCode === true){
+    return phrase = 'I love code'
+} else {return phrase = 'I do not love to code'
+    }
+}
+checkLovesCode(lovesCode);
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
